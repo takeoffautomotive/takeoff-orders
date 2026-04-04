9dc1eeb55f101ea9427fa3f5d68292a4337ab8bc
takeoffautomotive <takeoffautomotive@gmail.com>
2026-04-03 23:52:41 -0400

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: '#e63c2f',
+          orange: '#ff6b35',
+        },
+        dark: {
+          900: '#0f0f0f',
+          800: '#1a1a1a',
+          700: '#242424',
+          600: '#2e2e2e',
+          500: '#3a3a3a',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
